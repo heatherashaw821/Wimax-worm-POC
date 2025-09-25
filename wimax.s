e735797a8a88f97c3d822b5ff5ef956bbc0f39d4
Heather Shaw <aaronryool@gmail.com>
2025-09-25 02:53:14 -0400

    Update wimax.s

--- wimax.s
+++ wimax.s
@@ -1,9 +1,4 @@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@ THE SCOTCH-WARE LICENSE (Revision 0):
-@ <aaronryool@gmail.com> wrote this file. As long as you retain this notice you
-@ can do whatever you want with this stuff. If we meet some day, and you think
-@ this stuff is worth it, you can buy me a shot of scotch in return
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 .section .text
 .global _start
 .code 32
